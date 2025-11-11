f2cb5a7162d736e0c9e25e3fa9400cfbcc9d6c08
WellingthonSchuh <140724607+WellingthonSchuh@users.noreply.github.com>
2025-11-11 09:00:18 -0300

    add files

---
+++ validacao.js
@@ -0,0 +1,59 @@
+function meuScopo(){
+    const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado');
+
+    function recebeEventoForm (evento){
+        evento.preventDefault();
+        const cpfRecebido = form.querySelector('#cpf');
+        const cpf = new ValidaCPF(cpfRecebido.value);
+
+        function ValidaCPF(cpfEnviado){
+            Object.defineProperty(this, 'cpfLimpo', {
+                enumerable: true,
+                get: function(){
+                return cpfEnviado.replace(/\D+/g, '');
+                }
+            });
+        }
+    
+        ValidaCPF.prototype.valida = function(){
+            if(typeof this.cpfLimpo === 'undefined') return false;
+            if(this.cpfLimpo.length !== 11) return false;
+            if(this.isSequencia()) return false
+    
+            const cpfParcial = this.cpfLimpo.slice(0, -2);
+            const digito1 = this.criaDigito(cpfParcial);
+            const digito2 = this.criaDigito(cpfParcial + digito1);
+
+            const novoCPF = cpfParcial + digito1 + digito2;
+            return novoCPF === this.cpfLimpo;
+        }
+
+        ValidaCPF.prototype.criaDigito = function(cpfParcial){
+            const cpfArray = Array.from(cpfParcial);
+            let regressivo = cpfArray.length + 1;
+            const total = cpfArray.reduce((ac, val) => {
+                ac += (regressivo * Number(val))
+                regressivo--;
+                return ac;
+            },0);
+            const digito = 11 - (total % 11);
+            return digito > 9 ? '0' : String(digito);
+        };
+
+        ValidaCPF.prototype.isSequencia = function(){
+            const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
+            return sequencia === this.cpfLimpo;
+        };
+
+        
+        if(cpf.valida()){
+            resultado.innerHTML = `<div class='valid'>CPF valido</div>`;
+        }else{ 
+            resultado.innerHTML = `<div class='invalid'>CPF invalido</div>`;
+        }
+    }
+form.addEventListener('submit', recebeEventoForm);
+}
+
+meuScopo();
